0bbaf9cab521268a4081713dc4d71a3bf319f28f
mprasanna <prasannakumaran30@gmail.com>
2026-06-17 15:48:51 +0530

    Initialize application module

---
+++ app/app.py
@@ -0,0 +1,14 @@
+"""
+ISL to Kannada Translator
+
+Application Entry Point
+
+Author: Your Name
+"""
+
+def main():
+    print("ISL to Kannada Translator")
+
+
+if __name__ == "__main__":
+    main()
