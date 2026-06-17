285d8344df677d9ad2ae5f55c391f1a9b3c4ba54
mprasanna <prasannakumaran30@gmail.com>
2026-06-17 16:51:27 +0530

    Add centralized application configuration

---
+++ app/configs/config.py
@@ -0,0 +1,13 @@
+# Application Configuration
+
+CAMERA_INDEX = 0
+
+WINDOW_NAME = "ISL to Kannada Translator"
+
+MODEL_PATH = "model.pkl"
+
+FONT_PATH = "assets/fonts/NotoSansKannada-Regular.ttf"
+
+CONFIDENCE_THRESHOLD = 0.28
+
+HISTORY_LENGTH = 9
